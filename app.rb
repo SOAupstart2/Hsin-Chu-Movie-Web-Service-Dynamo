5aeb8be6ceabf828736df397b34ae74ebdca9c77
James Uanhoro <stonegold546@gmail.com>
2015-10-24 22:30:58 +0800

    Added a couple of sample routes
    - More for demonstration than substance

--- app.rb
+++ app.rb
@@ -1,8 +1,21 @@
 require 'sinatra'
+require 'kandianying'
 
 # Web Service for Hsinchu cinema
 class HsinchuMoveWebService < Sinatra::Base
   get '/' do
-    'Welcome to our API'
+    'Welcome to our API v1. Here\'s '\
+    ' <a href="https://github.com/SOAupstart2/Hsin-Chu-Movie-Web-Service">'\
+    'our github homepage</a>.'
+  end
+
+  get '/api/v1/list-movies/?' do
+    %w('0005', '0012').map do |cinema_id|
+      HsinChuMovie.new(cinema_id).movie_table_output
+    end
+  end
+
+  get '/api/v1/list-movies/:cinema_id.json' do
+    HsinChuMovie.new(params['cinema_id']).movie_table_output
   end
 end
