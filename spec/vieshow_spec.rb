840af56a5f82fcc7ebdf18c7f02a2dba3448e77b
James Uanhoro <stonegold546@gmail.com>
2015-11-29 18:32:04 +0800

    Added a test for root

--- spec/vieshow_spec.rb
+++ spec/vieshow_spec.rb
@@ -2,6 +2,11 @@ require_relative 'spec_helper'
 
 describe 'Getting the root of the service' do
   it 'should return ok' do
+    get '/'
+    last_response.must_be :ok?
+  end
+
+  it 'should return ok' do
     get '/api/v1'
     last_response.must_be :ok?
     last_response.body.must_match(/api v/i, /welcome/i)
