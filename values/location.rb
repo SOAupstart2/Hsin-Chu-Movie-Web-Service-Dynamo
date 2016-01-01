1bb17ebbf855eef57bc71b5fb97415ba0f218135
James Uanhoro <stonegold546@gmail.com>
2016-01-01 15:56:56 +0800

    Updated location value object to include places in Taiwan other than Hsinchu:
    - kaohsiung
    - taichung
    - tainan
    - hsinchu
    - taipei
    - new taipei city
    - toufen
    - pingtung
    - kinmen

--- values/location.rb
+++ values/location.rb
@@ -1,8 +1,23 @@
 LOCATION = {
-  'hsinchu' => {
-    'vieshow' => %w(05 12),
-    'ambassador' => %w(38897fa9-094f-4e63-9d6d-c52408438cb6)
-  }
+  'kaohsiung' => { 'vieshow' => %w(01),
+                   'ambassador' => %w(ec07626b-b382-474e-be39-ad45eac5cd1c) },
+  'taichung' => { 'vieshow' => %w(02 03 11) },
+  'tainan' => { 'vieshow' => %w(04 13),
+                'ambassador' => %w(ace1fe19-3d7d-4b7c-8fbe-04897cbed08c) },
+  'hsinchu' => { 'vieshow' => %w(05 12),
+                 'ambassador' => %w(38897fa9-094f-4e63-9d6d-c52408438cb6) },
+  'taipei' => { 'vieshow' => %w(06 08 09),
+                'ambassador' => %w(84b87b82-b936-4a39-b91f-e88328d33b4e
+                                   5c2d4697-7f54-4955-800c-7b3ad782582c
+                                   453b2966-f7c2-44a9-b2eb-687493855d0e
+                                   9383c5fa-b4f3-4ba8-ba7a-c25c7df95fd0) },
+  'new taipei city' => {
+    'vieshow' => %w(10),
+    'ambassador' => %w(357633f4-36a4-428d-8ac8-dee3428a5919
+                       3301d822-b385-4aa8-a9eb-aa59d58e95c9) },
+  'toufen' => { 'vieshow' => %w(14) },
+  'pingtung' => { 'ambassador' => %w(41aae717-4464-49f4-ac26-fec2d16acbd6) },
+  'kinmen' => { 'ambassador' => %w(65db51ce-3ad5-48d8-8e32-7e872e56aa4a) }
 }
 
 # Class for location
--- values/user_sanitizer.rb
+++ values/user_sanitizer.rb
@@ -2,6 +2,8 @@ require 'virtus'
 require 'active_model'
 
 ENG_CHI = %w(chinese english)
+LOCATIONS = %w(kaohsiung taichung tainan hsinchu taipei
+               new\ taipei\ city toufen pingtung kinmen)
 
 # Customized ActiveRecord Validations
 class DownCase < Virtus::Attribute
@@ -18,4 +20,5 @@ class UserSanitizer
   attribute :language, DownCase
   validates_inclusion_of :language, in: ENG_CHI
   attribute :location, DownCase
+  validates_inclusion_of :location, in: LOCATIONS
 end
